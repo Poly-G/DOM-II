4a2a7fb1e696619613b2462339d45d05a17c54f7
Poly Goveia <poly.goveia@gmail.com>
2019-02-01 00:12:55 -0500

    add button clicks and double clicks

--- js/index.js
+++ js/index.js
@@ -1,2 +1,23 @@
 // Your code goes here
+
+// VARIABLES
 let btn = document.querySelectorAll(".btn");
+
+// FUNCTIONS
+let colorGreen = (e) => {
+e.target.style.backgroundColor = 'green';
+};
+
+let colorBlue = (e) => {
+    e.target.style.backgroundColor = 'blue';
+};
+
+
+// EVENT LISTENERS
+for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('click', colorGreen);
+}
+
+for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('dblclick', colorBlue);
+}
